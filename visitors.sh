fbbfec4074ec35b5840a2864dc8e61612d9f5edf
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-05-10 16:59:28 +0200

    Update visitors.sh

--- visitors.sh
+++ visitors.sh
@@ -15,10 +15,7 @@ for LOG_FILE in $(ls -vr "$LOG_DIR"/access.log*); do
     fi
 
     DATES=($(eval "$CMD" | awk '{print $4}' | cut -c 2-12 | sort -u))
-
-    for i in "${!DATES[@]}"; do
-            DATE="${DATES[$i]}"
-            VISITORS_THIS_DAY=$(eval "$CMD" | grep $DATE | grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' | grep -i -v -e bot -e crawler -e xrawler -e $1 | wc -l)
-            echo "$DATE, $VISITORS_THIS_DAY"
-    done
+    DATE="${DATES[0]}"
+    VISITORS_THIS_DAY=$(eval "$CMD" | grep $DATE | grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' | grep  -v -i -E "(bot|crawler|xrawler|scan|$1)" | wc -l)
+    echo "$DATE, $VISITORS_THIS_DAY"
 done
