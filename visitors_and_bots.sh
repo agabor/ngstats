8945fb7780b7e634bc37c463b55a07a3d2ac1a4c
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-05-10 16:55:08 +0200

    Update visitors_and_bots.sh

--- visitors_and_bots.sh
+++ visitors_and_bots.sh
@@ -1,6 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-HUMANS=$(grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' /var/log/nginx/access.log |  grep -v -i -E "(bot|crawler|xrawler|scan)" | wc -l)
-BOTS=$(grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' /var/log/nginx/access.log |  grep -i -E "(bot|crawler|xrawler|scan)" | wc -l)
+LOG_DIR="/var/log/nginx"
+VISITORS=0
+CARRY=0
+CARRY_DATE=""
+echo "date, humans, bots"
 
-echo "Your website has been visitied by $HUMAN humans and $BOTS bots today"
+for LOG_FILE in $(ls -vr "$LOG_DIR"/access.log*); do
+
+    if [[ "$LOG_FILE" == *.gz ]]; then
+        CMD="zcat $LOG_FILE"
+    else
+        CMD="cat $LOG_FILE"
+    fi
+
+    DATES=($(eval "$CMD" | awk '{print $4}' | cut -c 2-12 | sort -u))
+
+    for i in "${!DATES[@]}"; do
+            DATE="${DATES[$i]}"
+            HUMANS=$(eval "$CMD" | grep $DATE | grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' |  grep -v -i -E "(bot|crawler|xrawler|scan)" | wc -l)
+            BOTS=$(eval "$CMD" | grep $DATE | grep -E 'GET (/termek/.*|/) HTTP/[21]\.[01]" 2.*' |  grep -i -E "(bot|crawler|xrawler|scan)" | wc -l)
+            echo "$DATE; $HUMAN; $BOTS"
+    done
+done
