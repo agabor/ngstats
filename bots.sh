29aa2800301ea696be961047cdcf5fe619461a11
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-05-11 10:40:30 +0200

    Update bots.sh

--- bots.sh
+++ bots.sh
@@ -1,4 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
+  
+LOG_DIR="/var/log/nginx"
 
-grep -i -E "(bot|crawler|xrawler|scan)" /var/log/nginx/access.log | grep compatible | awk -F "compatible;" '{print $NF}' | awk -F ")" '{print $1}' | sort | uniq
-grep -i -E "(bot|crawler|xrawler|scan)" /var/log/nginx/access.log | grep -v compatible | grep Google | awk -F "\"" '{print $(NF-1)}' | sort | uniq
+bots=()
+
+for LOG_FILE in $(ls -vr "$LOG_DIR"/access.log*); do
+
+    if [[ "$LOG_FILE" == *.gz ]]; then
+        CMD="zcat $LOG_FILE"
+    else
+        CMD="cat $LOG_FILE"
+    fi
+    #echo $LOG_FILE
+    bots=("${bots[@]}" $(eval "$CMD" |
+            awk -F'"' '/GET/ {print $6}' |
+            grep -E -i '(bot|crawler|xcrawler|scan)' |
+            awk -F 'compatible;' '{print $NF}' |
+            sed -r 's/Safari\/([0-9]+\.){1,2}[0-9]+//g' |
+            sed -r 's/Windows NT [0-9]+\.[0-9]+//g' |
+            sed -r 's/AppleWebKit\/([0-9]+\.){2}[0-9]+ \(KHTML, like Gecko\) Version\/([0-9]+\.){2}[0-9]+//g' |
+            sed 's/Macintosh//g' |
+            sed 's/Mozilla\/5\.0//g' |
+            sed 's/Intel//g' |
+            sed 's/x64//g' |
+            sed 's/x86_64//g' |
+            sed 's/Win64//g' |
+            sed 's/X11//g' |
+            sed 's/Linux//g' |
+            sed -r 's/Mac OS X ([0-9]+_){2}[0-9]+//g' |
+            sed -r 's/Chrome\/([0-9]+\.){3}[0-9]+//g' |
+            sed 's/http/ http/g' |
+            sed 's/[;()\+]//g' |
+            sed 's/[ \t]*$//' |
+            sed 's/^[ \t]*//' |
+            sed 's/[ \t]+/ /' |
+            sort -u |
+            sed 's/ /===/g'))
+done
+
+for bot in "${bots[@]}"; do echo $bot; done | sort -u | sed 's/===/ /g'
